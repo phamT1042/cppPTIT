0e18bcb3edf8fbe871683962a02b2fd2021fbe33
VanTienDesktop <pvantien203@gmail.com>
2023-01-10 09:04:30 +0700

    Cau truc diem

---
+++ CPP0501.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct Point {
+	double x, y;
+};
+double distance(Point a, Point b) {
+	double r = sqrt(pow(a.x - b.x, 2) + pow(a.y - b.y, 2));
+	return r;
+}
+void input(Point &a) {
+	cin >> a.x >> a.y;
+}
+int main(){
+    struct Point A, B;
+    int t;
+    cin>>t;
+    while(t--){
+        input(A); input(B);
+        cout << fixed << setprecision(4) << distance(A,B) << endl;
+    }
+    return 0;
+}
