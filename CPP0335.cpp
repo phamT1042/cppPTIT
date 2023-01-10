096dd6a7d5fe8027628df6beabe2c02b1a599212
VanTienDesktop <pvantien203@gmail.com>
2023-01-10 09:50:13 +0700

    Tìm số lớn nhất trong xâu

---
+++ CPP0335.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t; cin >> t;
+	while (t--) {
+		string s; cin >> s;
+		long long res = 0;
+		for (int i = 0; i < s.length(); i++) {
+			if (s[i] >= '0' && s[i] <= '9') {
+				string tmp = "";
+				while (s[i] >= '0' && s[i] <= '9') {
+					tmp += s[i++];
+				}
+				res = max(res, stoll(tmp));
+				i--;
+			}
+		}
+		cout << res << endl;
+	}
+}
